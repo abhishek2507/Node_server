ed5fb89d96fc86a22be87b85ec90c5b743c11890
Abhishek <abhishek2507@gmail.com>
2019-08-03 02:16:43 +0530

    Test

---
+++ app.js
@@ -0,0 +1,4 @@
+logger2 = require('./logger');
+logger =1;
+logger2.log('Hellobhai');
+console.log(logger);
---
+++ logger.js
@@ -0,0 +1,7 @@
+var url ='htt[://mylogger.io/log';
+function log(message){
+    //send a http request
+    console.log(message);
+}
+module.exports.log = log;
+
