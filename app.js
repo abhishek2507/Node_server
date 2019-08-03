6ff8ce076ef5f26cd94b891421f19fd1da2ec595
Abhishek <abhishek2507@gmail.com>
2019-08-03 17:36:54 +0530

    Node server

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-var http = require('http');
-var url = "https://samples.openweathermap.org/data/2.5/forecast?q=M%C3%BCnchen,DE&appid=495cf809c674e00a8d8affcf27011a60";
-
-var server = http.createServer(function(request,response){
-    //All logic goes here.
-    var request = require('request');
-    request(url,function(err,res,body){
-        var data = JSON.parse(body);
-        response.write("<h2>" + 'Temperature - : '+ data.main['temp'] + '<br>' + "</h2>");
-           /* response.write("<html><body><div id ='container'>");
-            response.write("<h1>" + 'City Name - :' + data['name'] + '<br>' + "</h1>");
-            response.write("<h2>" + 'Temperature - : '+ data.main['temp'] + '<br>' + "</h2>");
-            response.write("<h2>" + 'SunSet Time - : '+ new Date(data.sys['sunset']*1000)+"</h2>");
-            response.write("</div></body></html>"); */
-    console.log(body);
-   
-    response.end();    
-    });
-    
-
-}).listen(8081);
+var express = require('express');
+const app = express();
+const courses = [
+    { id: 1, name : 'courses1'},
+    { id: 2, name : 'courses2'},
+    { id: 3, name : 'courses3'},
+];
+app.get('/', (req,res) => {
+    res.send('Hello World');
+});
+app.get('/api/courses', (req,res) => {
+    res.send(courses);
+});
+app.get('/api/courses/:id', (req,res) => {
+   const course = courses.find(c => c.id === parseInt(req.params.id));
+   if(!course) res.status(404).send('The course with the given ID was not found.')//404 not found
+   res.send(course);
+});
+app.listen(8081)
