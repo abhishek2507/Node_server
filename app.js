f8ea581f0a3c6f40f68ddbeb6eebf4c9330b95c2
Abhishek <abhishek2507@gmail.com>
2019-08-05 19:42:40 +0530

    updatedpost request

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 const app = express();
+app.use(express.json());
 const courses = [
     { id: 1, name : 'courses1'},
     { id: 2, name : 'courses2'},
@@ -11,6 +12,14 @@ app.get('/', (req,res) => {
 app.get('/api/courses', (req,res) => {
     res.send(courses);
 });
+app.post('/api/courses', (req,res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.send(course)
+})
 app.get('/api/courses/:id', (req,res) => {
    const course = courses.find(c => c.id === parseInt(req.params.id));
    if(!course) res.status(404).send('The course with the given ID was not found.')//404 not found
