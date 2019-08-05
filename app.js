e531a7567a2722b5fe95984c084a3f4d86836f07
Abhishek <abhishek2507@gmail.com>
2019-08-05 21:51:35 +0530

    Mongo DB database added

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect("mongodb+srv://abhi655:Abhi@2507@abhiscluster-tyxhs.mongodb.net/test?retryWrites=true&w=majority",{
+    useNewUrlParser: true
+}, function(error){
+    if(error){
+        console.log(error);
+    }else{
+        console.log("Connected to the database");
+    }
+});
 app.use(express.json());
 const courses = [
     { id: 1, name : 'courses1'},
@@ -16,10 +27,10 @@ app.post('/api/courses', (req,res) => {
     const course = {
         id: courses.length + 1,
         name: req.body.name
-    }
+    };
     courses.push(course);
     res.send(course)
-})
+});
 app.get('/api/courses/:id', (req,res) => {
    const course = courses.find(c => c.id === parseInt(req.params.id));
    if(!course) res.status(404).send('The course with the given ID was not found.')//404 not found
